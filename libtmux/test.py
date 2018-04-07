e6b1384d6714f54cd9709371722f227cf5fa2719
Tony Narlock <tony@git-pull.com>
2018-04-07 08:21:52 -0500

    move libtmux.test helpers to numpy style

--- libtmux/test.py
+++ libtmux/test.py
@@ -21,35 +21,48 @@ fixtures_dir = os.path.realpath(os.path.join(current_dir, 'fixtures'))
 
 
 def retry(seconds=RETRY_TIMEOUT_SECONDS):
-    """Retry a block of code until a time limit or ``break``.
+    """
+    Retry a block of code until a time limit or ``break``.
 
-    .. code-block:: python
+    Parameters
+    ----------
+    seconds : int
+        Seconds to retry, defaults to ``RETRY_TIMEOUT_SECONDS``, which is
+        configurable via environmental variables.
 
-        while retry():
-            p = w.attached_pane
-            p.server._update_panes()
-            if p.current_path == pane_path:
-                break
+    Returns
+    -------
+    bool
+        True if time passed since retry() invoked less than seconds param.
 
+    Examples
+    --------
 
-    :param seconds: Seconds to retry, defaults to ``RETRY_TIMEOUT_SECONDS``,
-        which is configurable via environmental variables.
-    :type seconds: int
-    :rtype: void
+    >>> while retry():
+    ...      p = w.attached_pane
+    ...      p.server._update_panes()
+    ...      if p.current_path == pane_path:
+    ...          break
     """
     return (lambda: time.time() < time.time() + seconds)()
 
 
 def get_test_session_name(server, prefix=TEST_SESSION_PREFIX):
-    """Faker to create a session name that doesn't exist.
+    """
+    Faker to create a session name that doesn't exist.
 
-    :param server: libtmux server
-    :type server: :class:`libtmux.Server`
-    :param prefix: prefix for sessions (e.g. libtmux_). Defaults to
+    Parameters
+    ----------
+    server : :class:`libtmux.Server`
+        libtmux server
+    prefix : str
+        prefix for sessions (e.g. libtmux_). Defaults to
         ``TEST_SESSION_PREFIX``.
-    :type prefix: string
-    :rtype: string
-    :returns: Random session name guaranteed to not collide with current ones
+
+    Returns
+    -------
+    str
+        Random session name guaranteed to not collide with current ones.
     """
     while True:
         session_name = prefix + next(namer)
@@ -59,15 +72,23 @@ def get_test_session_name(server, prefix=TEST_SESSION_PREFIX):
 
 
 def get_test_window_name(session, prefix=TEST_SESSION_PREFIX):
-    """Faker to create a window name that doesn't exist.
+    """
+    Faker to create a window name that doesn't exist.
 
-    :param session: libtmux session
-    :type session: :class:`libtmux.Session`
-    :param prefix: prefix for sessions (e.g. libtmux_). Defaults to
-        ``TEST_SESSION_PREFIX``. ATM we reuse the test session prefix here.
-    :type prefix: string
-    :rtype: string
-    :returns: Random window name guaranteed to not collide with current ones
+    Parameters
+    ----------
+    session : :class:`libtmux.Session`
+        libtmux session
+    prefix : str
+        prefix for windows (e.g. libtmux_). Defaults to
+        ``TEST_SESSION_PREFIX``.
+
+        ATM we reuse the test session prefix here.
+
+    Returns
+    -------
+    str
+        Random window name guaranteed to not collide with current ones.
     """
     while True:
         window_name = prefix + next(namer)
@@ -78,22 +99,36 @@ def get_test_window_name(session, prefix=TEST_SESSION_PREFIX):
 
 @contextlib.contextmanager
 def temp_session(server, *args, **kwargs):
-    """Return a context manager with a temporary session.
-
-    e.g.::
-
-        with temp_session(server) as session:
-            session.new_window(window_name='my window')
-
-    The session will destroy itself upon closing with :meth:`Session.
-    kill_session()`.
+    """
+    Return a context manager with a temporary session.
 
     If no ``session_name`` is entered, :func:`get_test_session_name` will make
     an unused session name.
 
-    :args: Same arguments as :meth:`Server.new_session`
-    :yields: Temporary session
-    :rtype: :class:`Session`
+    The session will destroy itself upon closing with :meth:`Session.
+    kill_session()`.
+
+    Parameters
+    ----------
+    server : :class:`libtmux.Server`
+
+    Other Parameters
+    ----------------
+    args : list
+        Arguments passed into :meth:`Server.new_session`
+    kwargs : dict
+        Keyword arguments passed into :meth:`Server.new_session`
+
+    Yields
+    ------
+    :class:`libtmux.Session`
+        Temporary session
+
+    Examples
+    --------
+
+    >>> with temp_session(server) as session:
+    ...     session.new_window(window_name='my window')
     """
 
     if 'session_name' in kwargs:
@@ -113,12 +148,8 @@ def temp_session(server, *args, **kwargs):
 
 @contextlib.contextmanager
 def temp_window(session, *args, **kwargs):
-    """Return a context manager with a temporary window.
-
-    e.g.::
-
-        with temp_window(session) as window:
-            my_pane = window.split_window()
+    """
+    Return a context manager with a temporary window.
 
     The window will destroy itself upon closing with :meth:`window.
     kill_window()`.
@@ -126,9 +157,27 @@ def temp_window(session, *args, **kwargs):
     If no ``window_name`` is entered, :func:`get_test_window_name` will make
     an unused window name.
 
-    :args: Same arguments as :meth:`Session.new_window`
-    :yields: Temporary window
-    :rtype: :class:`Window`
+    Parameters
+    ----------
+    session : :class:`libtmux.Session`
+
+    Other Parameters
+    ----------------
+    args : list
+        Arguments passed into :meth:`Session.new_window`
+    kwargs : dict
+        Keyword arguments passed into :meth:`Session.new_window`
+
+    Yields
+    ------
+    :class:`libtmux.Window`
+        temporary window
+
+    Examples
+    --------
+
+    >>> with temp_window(session) as window:
+    ...     my_pane = window.split_window()
     """
 
     if 'window_name' not in kwargs:
@@ -151,10 +200,23 @@ def temp_window(session, *args, **kwargs):
 
 class EnvironmentVarGuard(object):
 
-    """Class to help protect the environment variable properly.  Can be used as
-    a context manager.
-      Vendorize to fix issue with Anaconda Python 2 not
-      including test module, see #121.
+    """Mock environmental variables safetly.
+
+    Helps rotect the environment variable properly.  Can be used as context
+    manager.
+
+    Notes
+    -----
+
+    Vendorized to fix issue with Anaconda Python 2 not including test module,
+    see #121 [1]_
+
+    References
+    ----------
+
+    .. [1] Just installed, "ImportError: cannot import name test_support".
+       GitHub issue for tmuxp. https://github.com/tmux-python/tmuxp/issues/121.
+       Created October 12th, 2015. Accessed April 7th, 2018.
     """
 
     def __init__(self):
