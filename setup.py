0daf9d1ce85fd09cb92bb8f0409a74d4bf27a028
Tony Narlock <tony@git-pull.com>
2019-08-17 10:10:06 -0500

    Use plain dict for project_urls

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ Manage tmux workspaces from JSON and YAML, pythonic API, shell completion.
 
 """
 import sys
-from collections import OrderedDict
 
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
@@ -45,14 +44,12 @@ setup(
     name=about['__title__'],
     version=about['__version__'],
     url=about['__github__'],
-    project_urls=OrderedDict(
-        (
-            ('Documentation', about['__docs__']),
-            ('Code', about['__github__']),
-            ('Issue tracker', about['__tracker__']),
-        )
-    ),
     download_url=about['__pypi__'],
+    project_urls={
+        'Documentation': about['__docs__'],
+        'Code': about['__github__'],
+        'Issue tracker': about['__tracker__'],
+    },
     license=about['__license__'],
     author=about['__author__'],
     author_email=about['__email__'],
