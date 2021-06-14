db3e48d9c74fb1cb9444ec019e9c0516f81fa1cb
Tony Narlock <tony@git-pull.com>
2021-06-14 10:47:24 -0500

    Compat: Remove python 2.7 specialize stuff

--- libtmux/_compat.py
+++ libtmux/_compat.py
@@ -1,98 +1,54 @@
 # -*- coding: utf8 -*-
 # flake8: NOQA
+import configparser
+import pickle
 import sys
+import urllib.parse as urlparse
+from collections.abc import MutableMapping
+from io import BytesIO, StringIO
+from string import ascii_lowercase
+from urllib.request import urlretrieve
 
 PY2 = sys.version_info[0] == 2
 
 _identity = lambda x: x
 
 
-if PY2:
-    unichr = unichr
-    text_type = unicode
-    string_types = (str, unicode)
-    integer_types = (int, long)
-    from urllib import urlretrieve
+unichr = chr
+text_type = str
+string_types = (str,)
+integer_types = (int,)
 
-    text_to_native = lambda s, enc: s.encode(enc)
+text_to_native = lambda s, enc: s
 
-    iterkeys = lambda d: d.iterkeys()
-    itervalues = lambda d: d.itervalues()
-    iteritems = lambda d: d.iteritems()
+iterkeys = lambda d: iter(d.keys())
+itervalues = lambda d: iter(d.values())
+iteritems = lambda d: iter(d.items())
 
-    from itertools import imap, izip
 
-    import ConfigParser as configparser
-    import cPickle as pickle
-    from cStringIO import StringIO as BytesIO
-    from StringIO import StringIO
+izip = zip
+imap = map
+range_type = range
 
-    range_type = xrange
+cmp = lambda a, b: (a > b) - (a < b)
 
-    cmp = cmp
+console_encoding = sys.__stdout__.encoding
 
-    input = raw_input
-    from collections import MutableMapping
-    from string import lower as ascii_lowercase
+implements_to_string = _identity
 
-    import urlparse
 
-    exec('def reraise(tp, value, tb=None):\n raise tp, value, tb')
-
-    def implements_to_string(cls):
-        cls.__unicode__ = cls.__str__
-        cls.__str__ = lambda x: x.__unicode__().encode('utf-8')
-        return cls
-
-    def console_to_str(s):
+def console_to_str(s):
+    """ From pypa/pip project, pip.backwardwardcompat. License MIT. """
+    try:
+        return s.decode(console_encoding, 'ignore')
+    except UnicodeDecodeError:
         return s.decode('utf_8', 'ignore')
 
 
-else:
-    unichr = chr
-    text_type = str
-    string_types = (str,)
-    integer_types = (int,)
-
-    text_to_native = lambda s, enc: s
-
-    iterkeys = lambda d: iter(d.keys())
-    itervalues = lambda d: iter(d.values())
-    iteritems = lambda d: iter(d.items())
-
-    import configparser
-    import pickle
-    from io import BytesIO, StringIO
-
-    izip = zip
-    imap = map
-    range_type = range
-
-    cmp = lambda a, b: (a > b) - (a < b)
-
-    input = input
-    import urllib.parse as urllib
-    import urllib.parse as urlparse
-    from string import ascii_lowercase
-    from urllib.request import urlretrieve
-
-    console_encoding = sys.__stdout__.encoding
-
-    implements_to_string = _identity
-
-    from collections.abc import MutableMapping
-
-    def console_to_str(s):
-        """ From pypa/pip project, pip.backwardwardcompat. License MIT. """
-        try:
-            return s.decode(console_encoding, 'ignore')
-        except UnicodeDecodeError:
-            return s.decode('utf_8', 'ignore')
-
-    def reraise(tp, value, tb=None):
-        if value.__traceback__ is not tb:
-            raise (value.with_traceback(tb))
-        raise value
+def reraise(tp, value, tb=None):
+    if value.__traceback__ is not tb:
+        raise (value.with_traceback(tb))
+    raise value
 
 
 number_types = integer_types + (float,)
