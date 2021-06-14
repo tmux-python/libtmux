97529990a305670a3cb69d76433e7b00dd65e527
Tony Narlock <tony@git-pull.com>
2021-06-14 11:20:29 -0500

    Tag v0.9.0

--- libtmux/__about__.py
+++ libtmux/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'libtmux'
 __package_name__ = 'libtmux'
-__version__ = '0.8.5'
+__version__ = '0.9.0'
 __description__ = 'scripting library / orm for tmux'
 __email__ = 'tony@git-pull.com'
 __author__ = 'Tony Narlock'
