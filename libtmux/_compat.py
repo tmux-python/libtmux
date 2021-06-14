943f1ec7ca4168eb2285c3f69b3aa64b009417a5
Tony Narlock <tony@git-pull.com>
2021-06-14 11:16:18 -0500

    chore: Remove more _compat

--- libtmux/_compat.py
+++ libtmux/_compat.py
@@ -1,40 +1,9 @@
 # -*- coding: utf8 -*-
 # flake8: NOQA
-import configparser
-import pickle
 import sys
-import urllib.parse as urlparse
 from collections.abc import MutableMapping
-from io import BytesIO, StringIO
-from string import ascii_lowercase
-from urllib.request import urlretrieve
-
-PY2 = sys.version_info[0] == 2
-
-_identity = lambda x: x
-
-
-unichr = chr
-text_type = str
-string_types = (str,)
-integer_types = (int,)
-
-text_to_native = lambda s, enc: s
-
-iterkeys = lambda d: iter(d.keys())
-itervalues = lambda d: iter(d.values())
-iteritems = lambda d: iter(d.items())
-
-
-izip = zip
-imap = map
-range_type = range
-
-cmp = lambda a, b: (a > b) - (a < b)
 
 console_encoding = sys.__stdout__.encoding
-
-implements_to_string = _identity
 
 
 def console_to_str(s):
@@ -51,11 +20,8 @@ def reraise(tp, value, tb=None):
     raise value
 
 
-number_types = integer_types + (float,)
-
-
 def str_from_console(s):
     try:
-        return text_type(s)
+        return str(s)
     except UnicodeDecodeError:
-        return text_type(s, encoding='utf_8')
+        return str(s, encoding='utf_8')
