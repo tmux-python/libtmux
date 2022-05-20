ca5fbaa18814a905516338c26452682cc4af1110
Tony Narlock <tony@git-pull.com>
2022-05-20 18:29:16 -0500

    Tag v0.12.0a1

--- libtmux/__about__.py
+++ libtmux/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "libtmux"
 __package_name__ = "libtmux"
-__version__ = "0.12.0a0"
+__version__ = "0.12.0a1"
 __description__ = "scripting library / orm for tmux"
 __email__ = "tony@git-pull.com"
 __author__ = "Tony Narlock"
