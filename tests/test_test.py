eb9742f2e81e48d08e4d6c81436f600463fa2702
Tony Narlock <tony@git-pull.com>
2022-05-20 05:02:55 -0500

    tests: Test retry_until with assert statements raises=False

--- tests/test_test.py
+++ tests/test_test.py
@@ -51,3 +51,37 @@ def test_function_times_out_no_rise():
     end = time()
 
     assert abs((end - ini) - 1.0) < 0.01
+
+
+def test_function_times_out_no_raise_assert():
+    ini = time()
+
+    def never_true():
+        return False
+
+    assert not retry_until(never_true, 1, raises=False)
+
+    end = time()
+
+    assert abs((end - ini) - 1.0) < 0.01
+
+
+def test_retry_three_times_no_raise_assert():
+    ini = time()
+    value = 0
+
+    def call_me_three_times():
+        nonlocal value
+
+        if value == 2:
+            return True
+
+        value += 1
+
+        return False
+
+    assert retry_until(call_me_three_times, 1, raises=False)
+
+    end = time()
+
+    assert abs((end - ini) - 0.1) < 0.01
