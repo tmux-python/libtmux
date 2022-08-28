bdcb72fb04b1f196b48e4247632c821b30262773
Tony Narlock <tony@git-pull.com>
2022-08-28 12:51:51 -0500

    Tag v0.15.0a0 (tmux_cmd's filtering of newlines only effect trailing)

--- libtmux/__about__.py
+++ libtmux/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "libtmux"
 __package_name__ = "libtmux"
-__version__ = "0.14.2"
+__version__ = "0.15.0a0"
 __description__ = "Typed scripting library / ORM / API wrapper for tmux"
 __email__ = "tony@git-pull.com"
 __author__ = "Tony Narlock"
