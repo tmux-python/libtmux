69e737711c3857d136c3c70cab080e34faf88465
Tony Narlock <tony@git-pull.com>
2022-09-11 21:55:23 -0500

    tests(pytest_plugin): Initial tests for plugin

---
+++ tests/test_pytest_plugin.py
@@ -0,0 +1,69 @@
+import textwrap
+
+import pytest
+
+import _pytest.pytester
+
+
+def test_plugin(
+    pytester: _pytest.pytester.Pytester,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    # Initialize variables
+    pytester.plugins = ["pytest_plugin"]
+    pytester.makefile(
+        ".ini",
+        pytest=textwrap.dedent(
+            """
+[pytest]
+addopts=-vv
+        """.strip()
+        ),
+    )
+    pytester.makeconftest(
+        textwrap.dedent(
+            r"""
+import pathlib
+import pytest
+
+@pytest.fixture(autouse=True)
+def setup(
+    request: pytest.FixtureRequest,
+) -> None:
+    pass
+    """
+        )
+    )
+    tests_path = pytester.path / "tests"
+    files = {
+        "example.py": textwrap.dedent(
+            """
+import pathlib
+
+def test_repo_git_remote_checkout(
+    session,
+) -> None:
+    assert session.name is not None
+
+    assert session._session_id == "$1"
+
+    new_window = session.new_window(attach=False, window_name="my window name")
+    assert new_window.name == "my window name"
+        """
+        )
+    }
+    first_test_key = list(files.keys())[0]
+    first_test_filename = str(tests_path / first_test_key)
+
+    # Setup: Files
+    tests_path.mkdir()
+    for file_name, text in files.items():
+        test_file = tests_path / file_name
+        test_file.write_text(
+            text,
+            encoding="utf-8",
+        )
+
+    # Test
+    result = pytester.runpytest(str(first_test_filename))
+    result.assert_outcomes(passed=1)
