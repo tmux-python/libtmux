908459daa698745b049c573bc608bee174ea62d6
Tony Narlock <tony@git-pull.com>
2022-09-19 22:26:24 -0500

    tests(conftest): Add request to doctest
    I still have to run locals().keys() to scope request into the doctest
    context, no idea why.

--- src/libtmux/conftest.py
+++ src/libtmux/conftest.py
@@ -26,6 +26,7 @@ def add_doctest_fixtures(
         doctest_namespace["window"] = session.attached_window
         doctest_namespace["pane"] = session.attached_pane
         doctest_namespace["pytester"] = pytester
+        doctest_namespace["request"] = request
 
 
 @pytest.fixture(autouse=True, scope="function")
