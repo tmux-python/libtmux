50569f094bde79aa430fde8d1451ebadfd3352f6
Tony Narlock <tony@git-pull.com>
2023-05-28 08:21:37 -0500

    Tag v0.22.1 (add back black dev dep) This is necessary until ruff replaces black

--- src/libtmux/__about__.py
+++ src/libtmux/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "libtmux"
 __package_name__ = "libtmux"
-__version__ = "0.22.0"
+__version__ = "0.22.1"
 __description__ = "Typed scripting library / ORM / API wrapper for tmux"
 __email__ = "tony@git-pull.com"
 __author__ = "Tony Narlock"
