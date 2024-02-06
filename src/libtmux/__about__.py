a24a67fddf84c926fd84ef4d29fb258ce7b942f8
Tony Narlock <tony@git-pull.com>
2024-02-06 11:18:00 -0600

    Tag v0.26.0 (linting and docstring improvements, see #514)

--- src/libtmux/__about__.py
+++ src/libtmux/__about__.py
@@ -1,7 +1,7 @@
 """Metadata package for libtmux."""
 __title__ = "libtmux"
 __package_name__ = "libtmux"
-__version__ = "0.25.0"
+__version__ = "0.26.0"
 __description__ = "Typed scripting library / ORM / API wrapper for tmux"
 __email__ = "tony@git-pull.com"
 __author__ = "Tony Narlock"
