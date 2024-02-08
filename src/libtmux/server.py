bbb5767bcc634c2e41172847ea98e264715a83fc
Tony Narlock <tony@git-pull.com>
2024-02-08 08:15:13 -0600

    feat(Server): Use OptionsMixin for Server.set_option, Server.show_option(s)

--- src/libtmux/server.py
+++ src/libtmux/server.py
@@ -18,6 +18,7 @@ import warnings
 from libtmux import exc, formats
 from libtmux._internal.query_list import QueryList
 from libtmux.common import tmux_cmd
+from libtmux.constants import OptionScope
 from libtmux.neo import fetch_objs
 from libtmux.pane import Pane
 from libtmux.session import Session
@@ -30,6 +31,7 @@ from .common import (
     WindowDict,
     session_check_name,
 )
+from .options import OptionsMixin
 
 if t.TYPE_CHECKING:
     import types
@@ -44,7 +46,7 @@ if t.TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-class Server(EnvironmentMixin):
+class Server(EnvironmentMixin, OptionsMixin):
     """:term:`tmux(1)` :term:`Server` [server_manual]_.
 
     - :attr:`Server.sessions` [:class:`Session`, ...]
@@ -116,6 +118,9 @@ class Server(EnvironmentMixin):
     """Unique child ID used by :class:`~libtmux.common.TmuxRelationalObject`"""
     formatter_prefix = "server_"
     """Namespace used for :class:`~libtmux.common.TmuxMappingObject`"""
+
+    default_option_scope: OptionScope | None = OptionScope.Server
+    """For option management."""
 
     def __init__(
         self,
