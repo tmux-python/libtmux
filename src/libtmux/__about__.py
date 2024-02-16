e7723a145436b94a617cd8986ef5d203b5816cf1
Tony Narlock <tony@git-pull.com>
2024-02-16 15:06:58 -0600

    Tag v0.30.1 (test module renamings)

--- src/libtmux/__about__.py
+++ src/libtmux/__about__.py
@@ -1,7 +1,7 @@
 """Metadata package for libtmux."""
 __title__ = "libtmux"
 __package_name__ = "libtmux"
-__version__ = "0.30.0"
+__version__ = "0.30.1"
 __description__ = "Typed scripting library / ORM / API wrapper for tmux"
 __email__ = "tony@git-pull.com"
 __author__ = "Tony Narlock"
