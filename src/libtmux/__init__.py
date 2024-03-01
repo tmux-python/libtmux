765d7000ec1052ca2fa9434745a843ae3d821abf
Sorin Sbarnea <ssbarnea@redhat.com>
2024-03-01 20:42:57 +0000

    Add implicit exports into __init__.py
    Fixes: #530

--- src/libtmux/__init__.py
+++ src/libtmux/__init__.py
@@ -13,3 +13,18 @@ from .pane import Pane
 from .server import Server
 from .session import Session
 from .window import Window
+
+__all__ = (
+    "Pane",
+    "Server",
+    "Session",
+    "Window",
+    "__author__",
+    "__copyright__",
+    "__description__",
+    "__email__",
+    "__license__",
+    "__package_name__",
+    "__title__",
+    "__version__",
+)
