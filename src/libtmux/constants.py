76cec6f9506a99a0839fb8021e7e17a0c10787cb
Tony Narlock <tony@git-pull.com>
2024-03-10 07:01:50 -0500

    constants(PaneDirection): Add enum and map for split-window `direction`

--- src/libtmux/constants.py
+++ src/libtmux/constants.py
@@ -32,3 +32,21 @@ WINDOW_DIRECTION_FLAG_MAP: t.Dict[WindowDirection, str] = {
     WindowDirection.Before: "-b",
     WindowDirection.After: "-a",
 }
+
+
+class PaneDirection(enum.Enum):
+    """Used for *adjustment* in :meth:`Pane.split()`."""
+
+    Above = "ABOVE"
+    Below = "BELOW"  # default with no args
+    Right = "RIGHT"
+    Left = "LEFT"
+
+
+PANE_DIRECTION_FLAG_MAP: t.Dict[PaneDirection, t.List[str]] = {
+    # -v is assumed, but for explicitness it is passed
+    PaneDirection.Above: ["-v", "-b"],
+    PaneDirection.Below: ["-v"],
+    PaneDirection.Right: ["-h"],
+    PaneDirection.Left: ["-h", "-b"],
+}
