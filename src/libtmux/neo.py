5918d9311ea33c1824d2bd9568014fc82dd6f093
Tony Narlock <tony@git-pull.com>
2024-03-16 08:27:16 -0500

    Obj: Add pane_at_{left,top,bottom,right}

--- src/libtmux/neo.py
+++ src/libtmux/neo.py
@@ -89,6 +89,10 @@ class Obj:
     next_session_id: t.Union[str, None] = None
     origin_flag: t.Union[str, None] = None
     pane_active: t.Union[str, None] = None  # Not detected by script
+    pane_at_bottom: t.Union[str, None] = None
+    pane_at_left: t.Union[str, None] = None
+    pane_at_right: t.Union[str, None] = None
+    pane_at_top: t.Union[str, None] = None
     pane_bg: t.Union[str, None] = None
     pane_bottom: t.Union[str, None] = None
     pane_current_command: t.Union[str, None] = None
