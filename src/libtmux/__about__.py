c148b7fd5bd83ce705145b6e287d775e99756b8d
Tony Narlock <tony@git-pull.com>
2025-02-02 05:51:27 -0600

    Tag v0.42.0 (tmux_cmd modernization via #560)

--- src/libtmux/__about__.py
+++ src/libtmux/__about__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __title__ = "libtmux"
 __package_name__ = "libtmux"
-__version__ = "0.41.0"
+__version__ = "0.42.0"
 __description__ = "Typed scripting library / ORM / API wrapper for tmux"
 __email__ = "tony@git-pull.com"
 __author__ = "Tony Narlock"
