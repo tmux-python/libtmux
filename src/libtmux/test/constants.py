65c15f3c28a6a3bcd88af815bc53bf4d2b066e9d
Tony Narlock <tony@git-pull.com>
2025-02-23 18:25:55 -0600

    test(constants) Documentation for variables

--- src/libtmux/test/constants.py
+++ src/libtmux/test/constants.py
@@ -4,6 +4,15 @@ from __future__ import annotations
 
 import os
 
+#: Prefix used for test session names to identify and cleanup test sessions
 TEST_SESSION_PREFIX = "libtmux_"
+
+#: Number of seconds to wait before timing out when retrying operations
+#: Can be configured via :envvar:`RETRY_TIMEOUT_SECONDS` environment variable
+#: Defaults to 8 seconds
 RETRY_TIMEOUT_SECONDS = int(os.getenv("RETRY_TIMEOUT_SECONDS", 8))
+
+#: Interval in seconds between retry attempts
+#: Can be configured via :envvar:`RETRY_INTERVAL_SECONDS` environment variable
+#: Defaults to 0.05 seconds (50ms)
 RETRY_INTERVAL_SECONDS = float(os.getenv("RETRY_INTERVAL_SECONDS", 0.05))
