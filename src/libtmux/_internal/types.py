2f11a06adc19962d48cc446468df146bb66fdb51
Tony Narlock <tony@git-pull.com>
2025-05-26 05:20:20 -0500

    types: Add `StrPath`

---
+++ src/libtmux/_internal/types.py
@@ -0,0 +1,19 @@
+"""Internal type annotations.
+
+Notes
+-----
+:class:`StrPath` and :class:`StrOrBytesPath` is based on `typeshed's`_.
+
+.. _typeshed's: https://github.com/python/typeshed/blob/5ff32f3/stdlib/_typeshed/__init__.pyi#L176-L179
+"""  # E501
+
+from __future__ import annotations
+
+import typing as t
+
+if t.TYPE_CHECKING:
+    from os import PathLike
+
+    from typing_extensions import TypeAlias
+
+StrPath: TypeAlias = "str | PathLike[str]"
