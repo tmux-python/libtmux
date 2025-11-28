0086e86955a8b04c5dff39ff1810477199e5ca7c
Tony Narlock <tony@git-pull.com>
2025-11-28 16:30:21 -0600

    tests: remove obsolete < 3.2 skipif markers
    why: tmux < 3.2 is now deprecated (TMUX_SOFT_MIN_VERSION = "3.2a").
    All version skipif markers for < 3.0, < 3.2, <= 3.1, and < 2.9 are
    now obsolete since all supported tmux versions have these features.
    
    what:
    - Remove skipif(has_lt_version("3.0")) from environment tests
    - Remove skipif(has_lt_version("3.2")) from window direction tests
    - Remove skipif(has_lt_version("2.9")) from resize tests
    - Remove skipif(has_lte_version("3.1")) from zoom tests
    - Clean up unused imports (has_lt_version, has_lte_version)

--- tests/legacy_api/test_window.py
+++ tests/legacy_api/test_window.py
@@ -10,7 +10,7 @@ import typing as t
 import pytest
 
 from libtmux import exc
-from libtmux.common import has_gte_version, has_lt_version, has_version
+from libtmux.common import has_gte_version, has_version
 from libtmux.constants import OptionScope
 from libtmux.pane import Pane
 from libtmux.server import Server
